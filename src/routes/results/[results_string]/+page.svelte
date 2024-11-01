adc1ad5f1c182e25373c1668e1f0124ba6ec7051
Chelsea Shaw <chelshaw.dev@gmail.com>
2024-11-01 11:09:34 -0500

    labels

--- src/routes/results/[results_string]/+page.svelte
+++ src/routes/results/[results_string]/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
 	import * as d3 from 'd3';
-	import { tick } from 'svelte';
-
-	interface Coord {
-		x: number;
-		y: number;
-	}
-
 	export let data;
-	let answers = { A: 1, B: 4, C: 3, D: 2, E: 5, F: 1, G: 2, H: 4 };
+
+	let answers = data.object;
 	const config = {
 		d: 500, // diameter of chart
 		px: 20, // padding horizontal each side
@@ -16,8 +10,6 @@
 		answerR: 18, // answer circle radius, should not be more than padding
 		features: Object.keys(answers),
 		ticks: [1, 2, 3, 4, 5]
-		// minAnswer: 1,
-		// maxAnswer: 5,
 	};
 
 	let radialScale = d3
@@ -30,25 +22,7 @@
 		.x((d: [number, number]) => d[0])
 		.y((d: [number, number]) => d[1]);
 
-	function getPathCoordinates(data_point: any): [number, number][] {
-		let coordinates = [];
-		for (var i = 0; i < config.features.length; i++) {
-			let ft_name = config.features[i];
-			let angle = Math.PI / 2 + (2 * Math.PI * i) / config.features.length;
-			coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
-		}
-		// [{ x, y }]
-		const final: [number, number][] = coordinates.map((c) => [c.x, c.y]); // [[x, y], [x, y]]
-		console.log({ data_point, final });
-		return final;
-	}
-	let answerCoords = getPathCoordinates(answers);
-
-	// // let svg = d3
-	// // 	.select('#chart')
-	// // 	.attr('width', config.d + 2 * config.px)
-	// // 	.attr('height', config.d + 2 * config.py);
-	// console.log({ svg });
+	// given the precise angle and value (1-5), returns {x, y} coordinate
 	function angleToCoordinate(angle: number, value: number) {
 		let x = Math.cos(angle) * radialScale(value);
 		let y = Math.sin(angle) * radialScale(value);
@@ -79,16 +53,44 @@
 		return angleToCoordinate(angle, 5);
 	}
 
-	// // svg
-	// // 	.selectAll('polygon')
-	// // 	.data(config.ticks)
-	// // 	.join((enter) =>
-	// // 		enter
-	// // 			.append('polygon')
-	// // 			.attr('points', (d) => tickToPolygon(d))
-	// // 			.attr('fill', 'none')
-	// // 			.attr('stroke', 'black')
-	// 	);
+	function drawAnswerShape(answers: Record<string, number>): string | null {
+		let coordinates: [number, number][] = [];
+		for (var i = 0; i < config.features.length; i++) {
+			let ft_name = config.features[i];
+			let angle = Math.PI / 2 + (2 * Math.PI * i) / config.features.length;
+			let { x, y } = angleToCoordinate(angle, answers[ft_name]);
+			coordinates.push([x, y]);
+		}
+		return lineHelper(coordinates);
+	}
+
+	// draws circles for score labels
+	function getCircleCoords({ answer, idx }: { answer: number; idx: number }) {
+		let angle = Math.PI / 2 + (2 * Math.PI * idx) / config.features.length;
+		return {
+			...angleToCoordinate(angle, answer),
+			answer
+		};
+	}
+	const formattedAnswers = config.features.reduce(
+		(
+			resultArray: {
+				feature: string;
+				answer: number;
+				xCoord: number;
+				yCoord: number;
+				idx: number;
+			}[],
+			feature,
+			idx
+		) => {
+			const answer = answers[feature];
+			const coords = getCircleCoords({ answer, idx });
+			resultArray.push({ feature, answer, xCoord: coords.x, yCoord: coords.y, idx });
+			return resultArray;
+		},
+		[]
+	);
 </script>
 
 <div id="container" style="position: relative; padding: 160px">
@@ -110,7 +112,11 @@
 				y2={featureLine(idx).y}
 			/>
 		{/each}
-		<path stroke-width="3" stroke="navy" fill="navy" opacity="0.8" d={lineHelper(answerCoords)} />
+		<path stroke-width="3" stroke="navy" fill="navy" opacity="0.8" d={drawAnswerShape(answers)} />
+		{#each formattedAnswers as ans}
+			<circle cx={ans.xCoord} cy={ans.yCoord} fill="navy" r={config.answerR}></circle>
+			<text x={ans.xCoord - 5} y={ans.yCoord + 3} fill="white">{ans.answer}</text>
+		{/each}
 	</svg>
 </div>
 
--- src/routes/results/[results_string]/+page.ts
+++ src/routes/results/[results_string]/+page.ts
@@ -11,7 +11,11 @@ export function load({ params }: { params: { results_string: string } }) {
 			// matches is an array of arrays with values like
 			// ['h2', 'h', '2', index: 14, ...]
 			return { key: m[1].toUpperCase(), value: m[2] };
-		})
+		}),
+		object: matches.reduce((prev: Record<string, number>, curr) => {
+			prev[curr[1].toUpperCase()] = parseInt(curr[2], 10);
+			return prev;
+		}, {})
 	};
 }
 
