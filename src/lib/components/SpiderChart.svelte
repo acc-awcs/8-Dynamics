fbf8c2015f16f74b68d79039b1ee2b4d3fa1e7a4
Chelsea Shaw <chelshaw.dev@gmail.com>
2024-11-04 15:26:24 -0600

    add labels, everything goes clockwise now

--- src/lib/components/SpiderChart.svelte
+++ src/lib/components/SpiderChart.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+	import dynamics from '$lib/dynamics';
 	import { line, scaleLinear } from 'd3';
-	export let answers: Record<string, number>;
-	export let features: string[];
+	let { answers, features }: { answers: Record<string, number>; features: string[] } = $props();
 
 	const config = {
 		d: 500, // diameter of chart
 		px: 20, // padding horizontal each side
 		py: 20, // padding vertical each side
 		answerR: 18, // answer circle radius, should not be more than padding
+		labelD: 160, // label diameter
 		ticks: [1, 2, 3, 4, 5]
 	};
 
@@ -19,9 +20,11 @@
 		.x((d: [number, number]) => d[0])
 		.y((d: [number, number]) => d[1]);
 
-	// given the precise angle and value (1-5), returns {x, y} coordinate
+	// given the precise angle in radians and value (1-5),
+	// returns {x, y} coordinate with padding accounted for
 	function angleToCoordinate(angle: number, value: number) {
-		let x = Math.cos(angle) * radialScale(value);
+		// multiplying by -1 makes the math count clockwise
+		let x = Math.cos(angle) * radialScale(value) * -1;
 		let y = Math.sin(angle) * radialScale(value);
 		return { x: config.px + (config.d / 2 + x), y: config.py + (config.d / 2 - y) };
 	}
@@ -88,29 +91,101 @@
 		},
 		[]
 	);
+
+	// calculates the offset of each label's centerpoint from the center of the chart
+	let labels = $derived.by(() => {
+		const numPoints = features.length;
+		const points = [];
+		const angleStep = (2 * Math.PI) / numPoints;
+		// not using radialScale because we want a radius outside those bounds
+		const radius = config.d / 2 + config.px + config.labelD / 2;
+
+		for (let i = 0; i < numPoints; i++) {
+			const angle = i * angleStep;
+			const x = radius * Math.sin(angle);
+			// multiply y by -1 so it maps clockwise
+			const y = radius * Math.cos(angle) * -1;
+			points.push({ offsetX: x, offsetY: y, text: dynamics[i] });
+		}
+		return points;
+	});
 </script>
 
-<svg
-	id="chart"
-	width={config.d + 2 * config.px}
-	height={config.d + 2 * config.py}
-	style="background: aliceblue; display: block; margin: 0 auto"
->
-	{#each config.ticks as tick}
-		<polygon fill="none" stroke="black" points={tickToPolygon(tick)} />
-	{/each}
-	{#each features as feature, idx}
-		<line
-			stroke="black"
-			x1={config.px + config.d / 2}
-			y1={config.py + config.d / 2}
-			x2={featureLine(idx).x}
-			y2={featureLine(idx).y}
-		/>
-	{/each}
-	<path stroke-width="3" stroke="navy" fill="navy" opacity="0.8" d={drawAnswerShape(answers)} />
-	{#each formattedAnswers as ans}
-		<circle cx={ans.xCoord} cy={ans.yCoord} fill="navy" r={config.answerR}></circle>
-		<text x={ans.xCoord - 5} y={ans.yCoord + 3} fill="white">{ans.answer}</text>
-	{/each}
-</svg>
+<div class="outer">
+	<svg id="chart" width={config.d + 2 * config.px} height={config.d + 2 * config.py}>
+		{#each config.ticks as tick}
+			<polygon fill="none" stroke="black" points={tickToPolygon(tick)} />
+		{/each}
+		{#each features as feature, idx}
+			<line
+				stroke="black"
+				x1={config.px + config.d / 2}
+				y1={config.py + config.d / 2}
+				x2={featureLine(idx).x}
+				y2={featureLine(idx).y}
+			/>
+		{/each}
+		<g id="answer">
+			<path stroke-width="3" opacity="0.8" d={drawAnswerShape(answers)} />
+			{#each formattedAnswers as ans}
+				<circle cx={ans.xCoord} cy={ans.yCoord} r={config.answerR}></circle>
+				<text x={ans.xCoord - 5} y={ans.yCoord + 3}>{ans.answer}</text>
+			{/each}
+		</g>
+	</svg>
+	<div class="labels-container" style={`width: ${config.labelD}px; height: ${config.labelD}px;`}>
+		{#each labels as label, idx}
+			<div
+				class="dynamic"
+				style={`width: ${config.labelD}px; height: ${config.labelD}px; transform: translate(${label.offsetX}px, ${label.offsetY}px)`}
+			>
+				Dynamic {idx + 1}
+				<br />
+				{label.text}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.outer {
+		position: relative;
+		background-color: var(--cream);
+	}
+	svg {
+		display: block;
+		margin: 0 auto;
+	}
+
+	/* ANSWER SHAPES */
+	#answer path {
+		fill: var(--moss);
+		stroke: var(--moss);
+	}
+	#answer circle {
+		fill: var(--moss);
+	}
+	#answer text {
+		fill: var(--cream);
+	}
+
+	/* LABELS */
+	.labels-container {
+		position: absolute;
+		display: block;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		transform-origin: center;
+	}
+	.dynamic {
+		position: absolute;
+		border-radius: 50%;
+		text-align: center;
+		display: flex;
+		align-items: center;
+		padding: 1rem;
+		box-sizing: border-box;
+		color: var(--charcoal);
+	}
+</style>
