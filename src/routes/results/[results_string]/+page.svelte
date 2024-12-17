ed5e1c065a759af1e95f7328bf34f63a7c5e4540
Emi Jackson <emij@umich.edu>
2024-12-17 13:17:01 -0500

    add spacing below 'Your Results' title on mobile

--- src/routes/results/[results_string]/+page.svelte
+++ src/routes/results/[results_string]/+page.svelte
@@ -109,10 +109,11 @@
 			bottom 0 right -100px,
 			bottom right;
 		background-size: 485px, 504px, 503px;
+		box-sizing: border-box;
 	}
 	main {
 		position: relative;
-		padding: 3rem;
+		padding: 1rem;
 		grid-template-columns: 1fr minmax(400px, 1fr);
 		grid-template-rows: min-content 1fr;
 		gap: 1em 3em;
@@ -130,6 +131,8 @@
 	h1 {
 		margin: 0;
 		padding: 0;
+		margin-top: 20px;
+		margin-bottom: 40px;
 	}
 	h2 {
 		font-family: 'adobe-garamond-pro', serif;
@@ -178,10 +181,14 @@
 	@media screen and (min-width: 900px) {
 		main {
 			display: grid;
+			padding: 3rem;
 		}
 		.actions {
 			flex-wrap: nowrap;
 			justify-content: flex-start;
+		}
+		h1 {
+			margin-bottom: 0px;
 		}
 	}
 </style>
