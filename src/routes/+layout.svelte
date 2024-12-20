0a39bc1cb8576fbc273ba95bf644518499e4d429
Chelsea Shaw <chelshaw.dev@gmail.com>
2024-12-20 09:38:09 -0600

    update image tag to FQDN

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,11 @@
 <svelte:head>
 	<title>The All We Can Save 8 Dynamics Quiz</title>
 	<meta name="description" content="Explore your dynamics of climate engagement" />
-	<meta property="og:image" content="%sveltekit.assets%/images/dynamics_static.png" />
+	<!-- Image tag must be FQDN -->
+	<meta
+		property="og:image"
+		content="https://dynamics-quiz.allwecansave.earth/images/dynamics_static.png"
+	/>
 </svelte:head>
 
 <slot />
